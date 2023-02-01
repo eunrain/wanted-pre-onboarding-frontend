11ec5ad475ad7eb4ca7515cd8d7138f24dd6979e
goeunrain <boniteterain@gmail.com>
2023-02-01 23:26:04 +0900

    initial commit

---
+++ src/shared/router.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+import Todo from "../pages/Todo";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
